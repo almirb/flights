be1eda833ff98d20faf841f933dbeb143eee2c82
Almir Bolduan <almir.bsi@gmail.com>
2025-12-16 20:36:31 -0300

    fix: return back to en language

--- fast_flights/core.py
+++ fast_flights/core.py
@@ -28,7 +28,7 @@ def get_flights_from_filter(
         "tfs": data.decode("utf-8"),
         "hl": "en",
         "tfu": "EgQIABABIgA",
-        "curr": "BRL",
+        "curr": currency,
     }
 
     if mode in {"common", "fallback"}:
